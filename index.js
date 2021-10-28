f5f1c20936b3eccf22e35bfa227d373c7816f675
YeniValle <yenivalle@YeniValles-MBP.attlocal.net>
2021-10-28 16:05:12 -0400

    Delete all leads

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 let myLeads = [];
+let leadsFromLocal = JSON.parse(localStorage.getItem("Lead"));
+
+if(leadsFromLocal){
+    myLeads = leadsFromLocal
+    renderLeads()
+}else{
+    console.log("No leads to show yet")
+}
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
+    localStorage.setItem("Lead", JSON.stringify(myLeads));
     inputEl.value = "";
     renderLeads()
 });
+
+deleteBtn.addEventListener("click", function(){
+    myLeads = [];
+    ulEl.innerHTML = "";
+    localStorage.clear();
+    leadsFromLocal = []
+})
 
 function renderLeads(){
     let listItems = "";
@@ -15,8 +33,10 @@ function renderLeads(){
         listItems += `<li><a href="https://www.google.com" target="_blank" rel="noopener noreferrer">${myLeads[i]}</a></li>` 
         
     } 
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
 }
+
+
 
 
 
